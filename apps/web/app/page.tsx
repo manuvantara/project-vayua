743909f3e77a58fa0ac09c53512eebab5e3c52fb
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-05-15 16:19:29 +0200

    feat: connect main app with docs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button, Header } from "ui";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <Link href='/docs'>
+        Go to docs
+      </Link>
     </>
   );
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination: "http://localhost:4000/docs"
+      },
+      {
+        source: "/docs/:path*",
+        destination: "http://localhost:4000/docs/:path*"
+      },
+    ]
+  }
 };
