afae33353b6ba952603d426b417a861d201a0106
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-05-16 00:33:54 +0200

    feat: compiler for wizard

--- apps/wizard/next.config.js
+++ apps/wizard/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+                path: false,
+            }
+        }
+        return config;
+    },
+}
 
 module.exports = nextConfig
