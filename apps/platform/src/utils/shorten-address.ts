5936475922d88d8c89c7f4257d104562d210092a
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-05-18 20:10:06 +0200

    feat(platform): add address shorter helper

---
+++ apps/platform/src/utils/shorten-address.ts
@@ -0,0 +1,16 @@
+import { getAddress } from "viem";
+
+// You can find more helpers here: https://github.com/TrueFiEng/useDApp/blob/master/packages/core/src/helpers/address.ts
+
+export function shortenString(str: string) {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+}
+
+export function shortenAddress(address: string): string {
+  try {
+    const formattedAddress = getAddress(address);
+    return shortenString(formattedAddress);
+  } catch {
+    throw new Error("Invalid input, address can't be parsed");
+  }
+}
