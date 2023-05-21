ad03e43de13eb79b3879c0bacc0998eac8a0b5cb
jusikXL <jusikxl@gmail.com>
2023-05-21 21:17:38 +0200

    feat(profile): hardhat config for theta network

--- packages/contracts/profile/hardhat.config.ts
+++ packages/contracts/profile/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    theta_testnet: {
+      url: `https://eth-rpc-api-testnet.thetatoken.org/rpc`,
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 365,
+    },
+    theta_mainnet: {
+      url: `https://eth-rpc-api.thetatoken.org/rpc`,
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 361,
+    },
+  },
 };
 
 export default config;
