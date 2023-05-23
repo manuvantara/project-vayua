d7f3ea61fda7b28babcae37f5101ca1ab567f135
jusikXL <jusikxl@gmail.com>
2023-05-23 14:52:20 +0200

    fix(platform): profile contract ABI for updated one

--- apps/platform/src/utils/abi/Profile_ABI.ts
+++ apps/platform/src/utils/abi/Profile_ABI.ts
@@ -1,6 +1,49 @@
 export const Profile_ABI = {
-  address: "0x6D82bA0C308156E5579Da9a2C3c00aA579D9e09c",
+  address: "0x4C36314a3027531101a03799b60c4f12E75A91B0",
   abi: [
+    {
+      type: "event",
+      anonymous: false,
+      name: "ProfileChanged",
+      inputs: [
+        {
+          type: "address",
+          name: "owner",
+          indexed: false,
+        },
+        {
+          type: "tuple",
+          name: "profile",
+          indexed: false,
+          components: [
+            {
+              type: "string",
+              name: "name",
+            },
+            {
+              type: "string",
+              name: "bio",
+            },
+            {
+              type: "string",
+              name: "avatar",
+            },
+            {
+              type: "string",
+              name: "location",
+            },
+            {
+              type: "string",
+              name: "website",
+            },
+            {
+              type: "string",
+              name: "extra",
+            },
+          ],
+        },
+      ],
+    },
     {
       type: "function",
       name: "profiles",
@@ -38,6 +81,19 @@ export const Profile_ABI = {
           name: "extra",
         },
       ],
+    },
+    {
+      type: "function",
+      name: "setExtra",
+      constant: false,
+      payable: false,
+      inputs: [
+        {
+          type: "string",
+          name: "_extra",
+        },
+      ],
+      outputs: [],
     },
     {
       type: "function",
