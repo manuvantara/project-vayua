a57813fcd5da3f91df1bd44108af96650a2a8138
jusikXL <jusikxl@gmail.com>
2023-05-30 20:41:37 +0200

    feat(platform): sorting proposals, fetching new proposals

--- apps/platform/src/components/Proposals.tsx
+++ apps/platform/src/components/Proposals.tsx
@@ -20,6 +20,19 @@ import { shortenAddress, shortenString } from "@/utils/shorten-address";
 
 const MIN_BLOCK_NUMBER = 21041027n;
 
+function getToBlockFromLocalStorage() {
+  return BigInt(JSON.parse(window.localStorage.getItem("toBlock") || "0"));
+}
+
+function getInitialBlockFromLocalStorage(currentBlockNumber: bigint) {
+  return BigInt(
+    JSON.parse(
+      window.localStorage.getItem("initialBlockNumber") ||
+        currentBlockNumber.toString()
+    )
+  );
+}
+
 export default function Proposals() {
   // get the governance contract address from route
   const router = useRouter();
@@ -52,8 +65,7 @@ export default function Proposals() {
     return parsedLogs;
   };
 
-  const fetchLogsPerCycle = async (toBlock: bigint) => {
-    const fromBlock = toBlock - 4999n;
+  const fetchLogsPerCycle = async (fromBlock: bigint, toBlock: bigint) => {
     try {
       const logsPerCycle = await publicClient.getLogs({
         address: govAddress,
@@ -71,23 +83,57 @@ export default function Proposals() {
     }
   };
 
-  const fetchLogs = async () => {
+  const fetchLogsDown = async () => {
     if (!block?.number) {
       return;
     }
 
-    const bigintStoredToBlock = BigInt(
-      JSON.parse(window.localStorage.getItem("toBlock") || "0")
-    );
-    let toBlock =
-      bigintStoredToBlock == 0n ? block.number : bigintStoredToBlock;
+    const currentBlock = block.number;
+    const toBlockStored = getToBlockFromLocalStorage();
+
+    let toBlock = toBlockStored == 0n ? currentBlock : toBlockStored;
+    let fromBlock = toBlock - 4999n;
 
     while (toBlock >= MIN_BLOCK_NUMBER) {
-      await fetchLogsPerCycle(toBlock);
+      await fetchLogsPerCycle(fromBlock, toBlock);
 
       toBlock -= 5000n;
+      fromBlock = toBlock - 4999n;
+
       window.localStorage.setItem(
         "toBlock",
+        JSON.stringify(toBlock.toString())
+      );
+    }
+  };
+
+  const fetchLogsUp = async () => {
+    if (!block?.number) {
+      return;
+    }
+
+    const currentBlock = block.number;
+    const toBlockStored = getToBlockFromLocalStorage();
+    const initialBlock = getInitialBlockFromLocalStorage(currentBlock);
+
+    if (toBlockStored == 0n) {
+      window.localStorage.setItem(
+        "initialBlockNumber",
+        JSON.stringify(currentBlock.toString())
+      );
+    }
+
+    let fromBlock = initialBlock;
+    while (fromBlock < currentBlock) {
+      const toBlock =
+        fromBlock + 5000n <= currentBlock ? fromBlock + 5000n : currentBlock;
+
+      await fetchLogsPerCycle(fromBlock + 1n, toBlock);
+
+      fromBlock += 5000n;
+
+      window.localStorage.setItem(
+        "initialBlockNumber",
         JSON.stringify(toBlock.toString())
       );
     }
@@ -101,7 +147,8 @@ export default function Proposals() {
   }, [publicClient]);
 
   useEffect(() => {
-    fetchLogs();
+    fetchLogsDown();
+    fetchLogsUp();
   }, [block]);
 
   useEffect(() => {
@@ -131,37 +178,43 @@ export default function Proposals() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {proposals.map((proposal) => (
-            <TableRow key={proposal.proposalId}>
-              <TableCell className="text-left">
-                {shortenString(proposal.proposalId)}
-              </TableCell>
-              <TableCell className="text-left">{proposal.voteStart}</TableCell>
-              <TableCell className="text-left">
-                {shortenAddress(proposal.proposer)}
-              </TableCell>
-              <TableCell className="text-left">
-                {proposal.description.slice(0, 100)}
-              </TableCell>
-              <TableCell className="text-left">
-                <Link
-                  href={{
-                    pathname: `${govAddress}/proposals/${proposal.proposalId}`,
-                    query: {
-                      description: proposal.description,
-                      proposer: proposal.proposer,
-                      voteStart: proposal.voteStart,
-                      targets: proposal.targets,
-                      values: proposal.values,
-                      // calldatas: proposal.calldatas,
-                    },
-                  }}
-                >
-                  <Button>More</Button>
-                </Link>
-              </TableCell>
-            </TableRow>
-          ))}
+          {proposals
+            .sort((a, b) =>
+              BigInt(a.voteStart) > BigInt(b.voteStart) ? -1 : 1
+            )
+            .map((proposal) => (
+              <TableRow key={proposal.proposalId}>
+                <TableCell className="text-left">
+                  {shortenString(proposal.proposalId)}
+                </TableCell>
+                <TableCell className="text-left">
+                  {proposal.voteStart}
+                </TableCell>
+                <TableCell className="text-left">
+                  {shortenAddress(proposal.proposer)}
+                </TableCell>
+                <TableCell className="text-left">
+                  {proposal.description.slice(0, 100)}
+                </TableCell>
+                <TableCell className="text-left">
+                  <Link
+                    href={{
+                      pathname: `${govAddress}/proposals/${proposal.proposalId}`,
+                      query: {
+                        description: proposal.description,
+                        proposer: proposal.proposer,
+                        voteStart: proposal.voteStart,
+                        targets: proposal.targets,
+                        values: proposal.values,
+                        // calldatas: proposal.calldatas,
+                      },
+                    }}
+                  >
+                    <Button>More</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </div>
