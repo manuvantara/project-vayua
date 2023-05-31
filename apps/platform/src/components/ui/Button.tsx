1b97274b93d7b4d8ba4f57e1b4a4fc0f091d7cb1
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-05-31 21:23:20 +0200

    fix(platform): revert pointer events none for disabled buttons

--- apps/platform/src/components/ui/Button.tsx
+++ apps/platform/src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/utils/class-merge";
 import Spinner from "@/components/ui/Spinner";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 aria-disabled:opacity-50 disabled:pointer-events-none aria-disabled:pointer-events-none disabled:cursor-not-allowed aria-disabled:cursor-not-allowed ring-offset-background aria-[busy=true]:opacity-50 aria-[busy=true]:pointer-events-none",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 aria-disabled:opacity-50 disabled:pointer-events-none aria-disabled:pointer-events-none ring-offset-background aria-[busy=true]:opacity-50 aria-[busy=true]:pointer-events-none",
   {
     variants: {
       variant: {
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 py-2 px-4",
-        sm: "h-9 px-3 rounded-md",
+        sm: "h-8 px-3 rounded-md",
         lg: "h-11 px-8 rounded-md",
       },
     },
