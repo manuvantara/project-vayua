2bc693d1b43e390d0f895c74cd9f68b5c7c4a009
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-05-31 21:22:16 +0200

    feat(platform): implement creation of multiple proposal actions
    This commit introduces a new feature to the platform, allowing users to create multiple proposal actions within a single proposal.

--- apps/platform/src/atoms/index.ts
+++ apps/platform/src/atoms/index.ts
@@ -2,3 +2,4 @@ export * from "./contract";
 export * from "./deployed";
 export * from "./steps";
 export * from "./editor";
+export * from "./actions";
--- apps/platform/src/pages/organisations/[organisationAddress]/proposals/new.tsx
+++ apps/platform/src/pages/organisations/[organisationAddress]/proposals/new.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps } from "next";
 import { Input } from "@/components/ui/Input";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Label } from "@/components/ui/Label";
 import {
   erc20ABI,
@@ -10,17 +10,18 @@ import {
 } from "wagmi";
 import { GOVERNOR_ABI } from "@/utils/abi/openzeppelin-contracts";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
-import { ArrowLeft, ChevronDown } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/router";
 import { useToast } from "@/components/ui/use-toast";
 import { encodeFunctionData } from "viem";
 import { useAtomValue } from "jotai";
-import { markdownEditorValueAtom } from "@/atoms";
+import { markdownEditorValueAtom, proposalActionsAtom } from "@/atoms";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import Link from "next/link";
 import Web3Button from "@/components/Web3Button";
 import MarkdownEditor from "@/components/MarkdownEditor";
+import NewProposalActions from "@/components/NewProposalActions";
 
 export default function NewProposalPage({
   organisationAddress,
@@ -34,15 +35,7 @@ export default function NewProposalPage({
   const [proposalName, setProposalName] = useState("");
   const markdownEditorValue = useAtomValue(markdownEditorValueAtom);
 
-  const [targetContractAddress, setTargetContractAddress] = useState("");
-  const [targetContractABI, setTargetContractABI] = useState([]);
-  const [targetFunctionId, setTargetFunctionId] = useState(0);
-  const [targetContractFunctions, setTargetContractFunctions] = useState<
-    { id: number; name: string; inputs: { name: string; type: string }[] }[]
-  >([]);
-  const [targetFunctionArguments, setTargetFunctionArguments] = useState<{
-    [key: string]: string;
-  } | null>();
+  const actions = useAtomValue(proposalActionsAtom);
 
   const {
     write,
@@ -62,144 +55,82 @@ export default function NewProposalPage({
     setProposalName(e.target.value);
   };
 
-  const handleContractABIUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-
-    if (!file) {
-      return;
-    }
-
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      const abi = reader.result as string;
-      try {
-        const parsedAbi = JSON.parse(abi);
-        setTargetContractABI(parsedAbi);
-
-        let functionId = 0;
-        const functions: {
-          id: number;
-          name: string;
-          inputs: { name: string; type: string }[];
-        }[] = parsedAbi
-          // TODO: Somehow type this, maybe use a library https://abitype.dev/
-          .filter((item: any) => item.type === "function")
-          .map((item: any) => {
-            return {
-              id: functionId++,
-              name: item.name,
-              inputs: item.inputs,
-            };
-          });
-
-        setTargetContractFunctions(functions);
-      } catch {
-        toast({
-          title: "Error",
-          description: "Invalid ABI",
-          variant: "destructive",
-        });
-      }
-    };
-    reader.readAsText(file);
-  };
-
-  const renderTargetFunctionArguments = useCallback(() => {
-    const targetFunction = targetContractFunctions.find(
-      (item) => item.id === targetFunctionId
-    );
-
-    if (!targetFunction) {
-      return null;
-    }
-
-    if (targetFunction.inputs.length === 0) {
-      return null; // No inputs, don't render the wrapper
-    }
-
-    return (
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold mb-2">Calldatas</h3>
-        <p className="text-sm text-muted-foreground mb-4">
-          The data for the function arguments you wish to send when the action
-          executes
-        </p>
-        {targetFunction.inputs.map((input) => {
-          const inputValue = targetFunctionArguments?.[input.name] || "";
-
-          const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue = e.target.value;
-            setTargetFunctionArguments((prevArguments) => ({
-              ...(prevArguments || {}),
-              [input.name]: newValue,
-            }));
-          };
-
-          return (
-            <div className="mt-2" key={input.name}>
-              <Label htmlFor={input.name}>{input.name}</Label>
-              <Input
-                id={input.name}
-                className="mt-2"
-                type="text"
-                value={inputValue}
-                onChange={handleChange}
-              />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }, [targetFunctionArguments, targetContractFunctions, targetFunctionId]);
-
-  useEffect(() => {
-    setTargetFunctionArguments({});
-  }, [targetFunctionId]);
-
   const handlePropose = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formattedDescription = `---\ntitle: ${proposalName}\n---\n ${markdownEditorValue}`;
 
-    let executableCode: `0x${string}` = "0x";
+    // const executableCode: `0x${string}`[] = [];
+    const proposalActions: {
+      targetContractAddress: `0x${string}`[];
+      value: bigint[];
+      executableCode: `0x${string}`[];
+    }[] = [];
 
-    const targetFunction = targetContractFunctions.find(
-      (item) => item.id === targetFunctionId
-    );
+    if (actions.length === 0) {
+      const executableCode = encodeFunctionData({
+        abi: erc20ABI,
+        functionName: "transfer",
+        args: [address as `0x${string}`, 0n],
+      });
 
-    try {
-      if (!targetFunction) {
-        executableCode = encodeFunctionData({
-          abi: erc20ABI,
-          functionName: "transfer",
-          args: [address as `0x${string}`, 0n],
-        });
-      } else {
-        const args = Object.values(targetFunctionArguments || {});
-        executableCode = encodeFunctionData({
-          abi: targetContractABI,
-          // For some reason the type is wrong here
-          functionName: targetFunction.name as any,
-          args,
-        });
+      proposalActions.push({
+        targetContractAddress: [organisationAddress],
+        value: [0n],
+        executableCode: [executableCode],
+      });
+    } else {
+      for (const action of actions) {
+        const {
+          targetContractABI,
+          targetFunctionId,
+          targetFunctionArguments,
+          targetContractAddress,
+        } = action.action;
+
+        const targetContractFunctions = targetContractABI.filter(
+          (item) => item.type === "function"
+        );
+
+        if (!targetContractFunctions) {
+          continue;
+        }
+
+        const targetFunction = targetContractFunctions[targetFunctionId];
+
+        try {
+          const args = Object.values(targetFunctionArguments || {});
+          const executableCode = encodeFunctionData({
+            abi: targetContractABI,
+            // For some reason the type is wrong here
+            functionName: targetFunction.name as any,
+            args,
+          });
+
+          proposalActions.push({
+            targetContractAddress: [targetContractAddress as `0x${string}`],
+            value: [0n],
+            executableCode: [executableCode],
+          });
+        } catch (e: any) {
+          toast({
+            title: "Error",
+            description: e.message,
+            variant: "destructive",
+          });
+          return;
+        }
       }
-
-      write({
-        args: [
-          [organisationAddress],
-          [0n],
-          [executableCode],
-          formattedDescription,
-        ],
-      });
-    } catch (e: any) {
-      toast({
-        title: "Error",
-        description: e.message,
-        variant: "destructive",
-      });
     }
+
+    write({
+      args: [
+        proposalActions.map((action) => action.targetContractAddress).flat(),
+        proposalActions.map((action) => action.value).flat(),
+        proposalActions.map((action) => action.executableCode).flat(),
+        formattedDescription,
+      ],
+    });
   };
 
   useEffect(() => {
@@ -274,56 +205,7 @@ export default function NewProposalPage({
         </div>
 
         <div>
-          <h3 className="text-2xl font-semibold tracking-tight">
-            Action{" "}
-            <span className="text-muted-foreground text-base font-normal">
-              (optional)
-            </span>
-          </h3>
-          <div className="mt-4">
-            <Label htmlFor="target">Target contract</Label>
-            <Input
-              id="target"
-              className="mt-2"
-              type="text"
-              placeholder="Enter target contract address"
-              value={targetContractAddress}
-              onChange={(e) => setTargetContractAddress(e.target.value)}
-            />
-          </div>
-          <div className="mt-4">
-            <Label htmlFor="abi">ABI</Label>
-            <Input
-              id="abi"
-              className="mt-2"
-              type="file"
-              accept=".json"
-              onChange={handleContractABIUpload}
-            />
-          </div>
-          {targetContractABI.length > 0 && (
-            <div className="mt-4">
-              <Label htmlFor="target-function">Target function</Label>
-              <div className="relative flex items-center mt-2 w-[200px]">
-                <select
-                  value={targetFunctionId}
-                  onChange={(e) => setTargetFunctionId(Number(e.target.value))}
-                  id="target-function"
-                  className="cursor-pointer h-10 w-full items-center justify-between rounded-md border appearance-none border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                >
-                  {targetContractFunctions.map((func) => (
-                    <option key={func.id} value={func.id}>
-                      {func.name}
-                    </option>
-                  ))}
-                </select>
-                <span className="inline-flex items-center absolute pointer-events-none right-3 w-4 h-4">
-                  <ChevronDown />
-                </span>
-              </div>
-            </div>
-          )}
-          {renderTargetFunctionArguments()}
+          <NewProposalActions />
         </div>
 
         <div className="flex justify-end">
