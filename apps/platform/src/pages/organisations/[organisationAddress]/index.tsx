57635ed70da4e59915cd11320d9593fe882f002b
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-06-02 14:07:17 +0200

    feat(platform): implement server-side redirect for non-organization search addresses

--- apps/platform/src/pages/organisations/[organisationAddress]/index.tsx
+++ apps/platform/src/pages/organisations/[organisationAddress]/index.tsx
@@ -1,29 +1,38 @@
 import type { GetServerSideProps } from "next";
 import Proposals from "@/components/Proposals";
 import OrganisationProfile from "@/components/OrganisationProfile";
-import { useContractRead } from "wagmi";
 import { GOVERNOR_ABI } from "@/utils/abi/openzeppelin-contracts";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { createPublicClient, http } from "viem";
+import { thetaTestnet } from "@/utils/chains/theta-chains";
 
 export default function OrganisationPage({
   organisationAddress,
 }: {
   organisationAddress: `0x${string}`;
 }) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const contractRead = useContractRead({
-    address: organisationAddress,
-    abi: GOVERNOR_ABI,
-    functionName: "token",
-  });
+  // const contractRead = useContractRead({
+  //   address: organisationAddress,
+  //   abi: GOVERNOR_ABI,
+  //   functionName: "token",
+  // });
 
-  useEffect(() => {
-    if (contractRead.isError) {
-      router.push("/404");
-    }
-  }, [contractRead.isError]);
+  // useEffect(() => {
+  //   if (contractRead.isError) {
+  //     router.replace(
+  //       {
+  //         pathname: "/404",
+  //         query: {
+  //           message: "Organisation not found",
+  //           description:
+  //             "It looks like the address you entered is not a valid organisation address.",
+  //         },
+  //       },
+  //       "/invalid-organisation"
+  //     );
+  //   }
+  // }, [contractRead.isError]);
 
   return (
     <div className="lg:grid lg:grid-cols-3 gap-5">
@@ -37,6 +46,33 @@ export default function OrganisationPage({
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   // Better than useRouter hook because on the client side we will always have the address
   const organisationAddress = params?.organisationAddress as `0x${string}`;
+
+  const publicClient = createPublicClient({
+    chain: thetaTestnet,
+    transport: http(),
+  });
+
+  try {
+    // If there is no token, the contract will throw an error and we will redirect to the 404 page
+    await publicClient.readContract({
+      address: organisationAddress,
+      abi: GOVERNOR_ABI,
+      functionName: "token",
+    });
+  } catch {
+    const query = new URLSearchParams({
+      message: "Organisation not found",
+      description:
+        "It looks like the address you entered is not a valid organisation address.",
+    });
+
+    return {
+      redirect: {
+        destination: `/invalid-organisation?${query}`,
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
