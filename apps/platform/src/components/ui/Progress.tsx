93b2ec8eed34015550b3e3039360126d84b35ebe
jusikXL <jusikxl@gmail.com>
2023-06-02 17:13:20 +0200

    fix(platform): display progress for max = 0

--- apps/platform/src/components/ui/Progress.tsx
+++ apps/platform/src/components/ui/Progress.tsx
@@ -17,26 +17,29 @@ type ProgressProps = React.ComponentPropsWithoutRef<
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   ProgressProps
->(({ className, value = 0, max = 100, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
+>(({ className, value = 0, max = 100, ...props }, ref) => {
+  max = max === 0 ? 100 : max;
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
       className={cn(
-        "h-full w-full flex-1 bg-primary transition-all",
-        props.indicatorClassName
+        "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+        className
       )}
-      style={{
-        transform: `translateX(-${100 - ((value as number) / max) * 100}%)`,
-      }}
-    />
-  </ProgressPrimitive.Root>
-));
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className={cn(
+          "h-full w-full flex-1 bg-primary transition-all",
+          props.indicatorClassName
+        )}
+        style={{
+          transform: `translateX(-${100 - ((value as number) / max) * 100}%)`,
+        }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
