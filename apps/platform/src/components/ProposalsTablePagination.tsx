ca31bdb8c1e67290d32573c3717f581e86310593
jusikXL <jusikxl@gmail.com>
2023-06-15 17:49:00 +0200

    fix: disappearance of proposals table pagination buttons

--- apps/platform/src/components/ProposalsTablePagination.tsx
+++ apps/platform/src/components/ProposalsTablePagination.tsx
@@ -17,14 +17,14 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="mt-5 flex items-center justify-between px-2 pb-5 sm:p-0">
-      <div className="flex items-center space-x-6 lg:space-x-8">
+      <div className="flex items-center space-x-4 p-4">
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
           Page {table.getState().pagination.pageIndex + 1} of{' '}
           {table.getPageCount()}
         </div>
         <div className="flex items-center space-x-2">
           <Button
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="h-8 w-8 p-0"
             disabled={!table.getCanPreviousPage()}
             onClick={() => table.setPageIndex(0)}
             variant="outline"
@@ -51,7 +51,7 @@ export function DataTablePagination<TData>({
             <ChevronRight className="h-4 w-4" />
           </Button>
           <Button
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="h-8 w-8 p-0"
             disabled={!table.getCanNextPage()}
             onClick={() => table.setPageIndex(table.getPageCount() - 1)}
             variant="outline"
