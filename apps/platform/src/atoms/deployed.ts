e3a7d2c9cd5bba43b482e2fb6354217c1ea712f3
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-06-26 15:23:10 +0200

    refactor: remove unused files

--- apps/platform/src/atoms/deployed.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai';
-
-type DeployedAddress = `0x${string}`;
-
-export const deployedTokenAddressAtom = atom<DeployedAddress | null>(null);
-export const deployedGovernorAddressAtom = atom<DeployedAddress | null>(null);
--- apps/platform/src/atoms/index.ts
+++ apps/platform/src/atoms/index.ts
@@ -1,5 +1,3 @@
 export * from './contract';
-export * from './deployed';
-export * from './steps';
 export * from './editor';
 export * from './actions';
