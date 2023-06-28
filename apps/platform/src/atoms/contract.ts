d7f443d661d9f8d5b1a0b0ddcd8a29ea8329874a
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-06-28 19:02:46 +0200

    feat: add contracts atom

--- apps/platform/src/atoms/contract.ts
+++ apps/platform/src/atoms/contract.ts
@@ -5,6 +5,8 @@ type Contract = {
   source: string;
 };
 
+type ContractMap = Map<'governanceContract' | 'tokenContract', Contract>;
+
 export const tokenTypeAtom = atom<'erc20' | 'erc721'>('erc20');
 
 export const tokenContractAtom = atom<Contract>({
@@ -14,4 +16,13 @@ export const tokenContractAtom = atom<Contract>({
 export const governanceContractAtom = atom<Contract>({
   name: '',
   source: '',
+});
+
+export const contractsAtom = atom<ContractMap>((get) => {
+  const tokenContract = get(tokenContractAtom);
+  const governanceContract = get(governanceContractAtom);
+  const contracts = new Map();
+  contracts.set('tokenContract', tokenContract);
+  contracts.set('governanceContract', governanceContract);
+  return contracts;
 });
