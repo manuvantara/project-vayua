c6411ceaee60a82d85ca970f71eefecd6895c304
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-06-28 19:29:21 +0200

    feat: add sentry integration

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -30,4 +30,39 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    org: 'manuvantara',
+
+    project: 'javascript-nextjs',
+    // Suppresses source map uploading logs during build
+    silent: true,
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: process.env.NODE_ENV === 'production',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+  },
+);
